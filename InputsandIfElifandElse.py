8128dc6d5d8274b85d3f7e4122ffbdac85926e56
xxmlg1783xx <92173503+xxmlg1783xx@users.noreply.github.com>
2023-05-29 20:50:48 -0500

    Create InputsandIfElifandElse.py

---
+++ InputsandIfElifandElse.py
@@ -0,0 +1,17 @@
+#So now We can use inputs more often
+#Like this
+legendName = input("Legend Name > ")
+#Now an if statement
+if legendName == "Flematrix":
+  print("All enemies instantly die and you have found the GOD NAME CODE"
+elif legendName == "Name"
+  print("Haha real funny.")
+else:
+  print("Nice name")
+
+#So now with this I'll explain it
+#If checks for a certain condition
+#elif is the inbetween of if and else
+#else is basically the statement giving a diffrent outcome if the condition is not met
+#(Go to next file)
+        
