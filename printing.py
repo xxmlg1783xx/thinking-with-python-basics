90bbd39b0cb72094e997058c0790de8b24e86b3e
xxmlg1783xx <92173503+xxmlg1783xx@users.noreply.github.com>
2023-05-29 20:36:26 -0500

    Create printing.py
    The first lesson of this. All of these files are meant for teaching reasons but can be altered as they are an open source thing (for teaching reasons)

---
+++ printing.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+#Ah yes the most basic yet most iconic Python Line 
+#It's even the first line of code you see when you create a python file
+#But on to the lesson, the print command is used lots while programming with python. Its been used to print varibles and much more
+#Heres an exampe
+varExample = 2
+print(varExample)
+#Try running it and see what happens
+#So after that you can do more with print, like this!
+name = input("Name > ")
+#Input is a command which allows you to set values after programming it if your using varibles
+#But let's be honest that what most people use it for anyway
+print(name)
+#Okay with that out the way we can do more with python (In the next lesson)
+#Open the next file to learn more
